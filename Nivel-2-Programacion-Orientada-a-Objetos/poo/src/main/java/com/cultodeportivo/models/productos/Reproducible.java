2573936119d66f0c3cea5a8cea65b2c2e189f095
FepDev25 <lamediatricolor@gmail.com>
2025-03-04 23:46:35 -0500

    Empezando parte poo

---
+++ Nivel-2-Programacion-Orientada-a-Objetos/poo/src/main/java/com/cultodeportivo/models/productos/Reproducible.java
@@ -0,0 +1,11 @@
+package com.cultodeportivo.models.productos;
+
+public interface Reproducible {
+    void reproducir();
+    void pausar();
+    void rebobinar();
+    void adelantar();
+    void siguiente();
+    void anterior();
+    void mostrarInfo();
+}
